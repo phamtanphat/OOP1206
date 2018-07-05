d73375b1f2c0693327cf48a65a4b44f85eb57054
thuviengithub0208 <khoaphp@yahoo.com>
2018-07-05 18:32:57 +0700

    "Tinh da hinh : Phuong thuc nap chong"

--- app/src/main/java/com/ptp/phamtanphat/oop1206/Lopnguoicon.java
+++ app/src/main/java/com/ptp/phamtanphat/oop1206/Lopnguoicon.java
@@ -2,4 +2,5 @@ package com.ptp.phamtanphat.oop1206;
 
 public class Lopnguoicon extends LopCha{
     public Integer cannang;
+
 }
--- app/src/main/java/com/ptp/phamtanphat/oop1206/MainActivity.java
+++ app/src/main/java/com/ptp/phamtanphat/oop1206/MainActivity.java
@@ -7,24 +7,35 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Override : Phuong thuc ghi de
+    //OverLoad : Phuong thuc nap chong
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        LopCha nguoicha = new LopCha();
-        nguoicha.ten = "Ong Nguyen Van A";
-        nguoicha.tuoi = 50;
-        nguoicha.Tinhtuoi();
-
-        Lopnguoicon nguoicon = new Lopnguoicon();
-        nguoicon.ten = "Con Nguyen Van B";
-        nguoicon.tuoi = 10;
-        nguoicon.cannang = 30;
-        nguoicon.Tinhtuoi();
-
-        Log.d("BBBB",nguoicon.ten);
-        Log.d("BBBB",nguoicon.tuoi + "");
-        Log.d("BBBB",nguoicon.cannang + "");
+//        LopCha nguoicha = new LopCha();
+//        nguoicha.ten = "Ong Nguyen Van A";
+//        nguoicha.tuoi = 50;
+//        nguoicha.Tinhtuoi();
+//
+//        Lopnguoicon nguoicon = new Lopnguoicon();
+//        nguoicon.ten = "Con Nguyen Van B";
+//        nguoicon.tuoi = 10;
+//        nguoicon.cannang = 30;
+//        nguoicon.Tinhtuoi();
+//
+//        Log.d("BBBB",nguoicon.ten);
+//        Log.d("BBBB",nguoicon.tuoi + "");
+//        Log.d("BBBB",nguoicon.cannang + "");
+        //Phuong thuc nhap chong
+        Tinhtoan(5 ,10);
+        Tinhtoan(4);
+    }
+    public void Tinhtoan(Integer num1 , Integer num2){
+        Toast.makeText(this, (num1 + num2) + "", Toast.LENGTH_SHORT).show();
+    }
+    public void Tinhtoan(Integer num1 ){
+        Toast.makeText(this, (num1 / 2) + "", Toast.LENGTH_SHORT).show();
     }
 }
