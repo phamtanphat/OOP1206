da40cfc25fd997d5ae737965d93bcba9c57752b4
thuviengithub0208 <khoaphp@yahoo.com>
2018-07-05 17:55:17 +0700

    Initial commit

---
+++ app/src/main/java/com/ptp/phamtanphat/oop1206/Lopdongvat.java
@@ -0,0 +1,14 @@
+package com.ptp.phamtanphat.oop1206;
+
+public class Lopdongvat {
+    private String ten;
+    private Integer tuoi;
+
+    public void setTen(String tendongvat){
+        ten = tendongvat;
+    }
+    public String getTen(){
+        return ten;
+    }
+
+}
---
+++ app/src/main/java/com/ptp/phamtanphat/oop1206/MainActivity.java
@@ -0,0 +1,23 @@
+package com.ptp.phamtanphat.oop1206;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        Class : La mot tap cac doi tuong giong ve thuoc tinh hay hanh vi
+
+        Lopdongvat concho = new Lopdongvat();
+        concho.setTen("Con Cho");
+        Toast.makeText(this, concho.getTen(), Toast.LENGTH_SHORT).show();
+
+//        Lopdongvat conbo = new Lopdongvat();
+//        conbo.ten = "con Bo";
+//        conbo.tuoi = 5;
+    }
+}
